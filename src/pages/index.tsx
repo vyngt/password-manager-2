761e70c8cded76c7aabdc5e4bcb24e4a23361ae8
TheVy <ntvy2k@gmail.com>
2023-04-12 00:22:57 +0700

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -65,19 +66,14 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link href="/login" className={styles.card}>
             <h2 className={inter.className}>
               Learn <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
-          </a>
+          </Link>
 
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -110,5 +106,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { ChangeEvent } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import { useState } from "react";
+
+export default function Login() {
+  const [input_name, set_input_name] = useState("");
+  const [display_output, set_display_output] = useState("");
+
+  const input_name_handler = (e: ChangeEvent<HTMLInputElement>) => {
+    set_input_name(e.target.value);
+  };
+
+  const perform_action = () => {
+    invoke("greet", { name: input_name })
+      .then((e) => set_display_output(e as string))
+      .catch(console.error);
+  };
+
+  return (
+    <>
+      <div>
+        <button>
+          <Link href="/">Main</Link>
+        </button>
+        <div>
+          <p>This is Login Page</p>
+        </div>
+        <div>
+          <p>
+            Text <span>{display_output}</span>
+          </p>
+        </div>
+        <form>
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Name"
+            name="name"
+            value={input_name}
+            onChange={input_name_handler}
+          />
+          <button type="button" onClick={perform_action}>
+            Send
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
