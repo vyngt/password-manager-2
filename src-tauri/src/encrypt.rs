209e32d953e02edb1d32f9108ed800a4897f867a
TheVy <ntvy2k@gmail.com>
2023-04-16 02:10:29 +0700

    encryptor

--- src-tauri/src/encrypt.rs
+++ src-tauri/src/encrypt.rs
@@ -1,2 +1,1 @@
-pub mod decryptor;
 pub mod encryptor;
--- src-tauri/src/encrypt/encryptor.rs
+++ src-tauri/src/encrypt/encryptor.rs
@@ -1,1 +1,17 @@
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
+pub fn encrypt_data(password: &str, text: &str) -> String {
+    let mc = new_magic_crypt!(password, 256);
+    let ciphertext = mc.encrypt_str_to_base64(text);
+
+    ciphertext
+}
+
+pub fn decrypt_data(password: &str, ciphertext: &str) -> String {
+    let mc = new_magic_crypt!(password, 256);
+    let plaintext = mc
+        .decrypt_base64_to_string(ciphertext)
+        .unwrap_or_else(|_| String::from(""));
+
+    plaintext
+}
