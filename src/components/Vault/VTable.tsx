17cd23f766f644a5ddf3517244567899db53295b
TheVy <ntvy2k@gmail.com>
2023-04-27 20:54:06 +0700

    copy, edit, delete

--- src/components/Vault/VTable.tsx
+++ src/components/Vault/VTable.tsx
@@ -12,9 +12,55 @@ export const VRow = ({
       <td className="whitespace-nowrap px-6 py-4">{item.url}</td>
       <td className="whitespace-nowrap px-6 py-4">{item.username}</td>
       <td className="whitespace-nowrap px-6 py-4">
-        <button onClick={() => operator.update(item)}>Copy</button>|
-        <button onClick={() => operator.update(item)}>Edit</button>|
-        <button onClick={() => operator.delete(item)}>Delete</button>|
+        <button onClick={() => operator.delete(item)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 fill-[#bf78de] hover:fill-[#da6885]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+
+        <button onClick={() => operator.update(item)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 fill-[#16d16a] hover:fill-[#d94ea1]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+            />
+          </svg>
+        </button>
+        <button onClick={() => operator.update(item)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 fill-[#16d1ce] hover:fill-[#6ca3c5]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+            />
+          </svg>
+        </button>
       </td>
     </tr>
   );
