b15f9c59ab4ad8e37d0e6a2cf67c34a6458105fb
TheVy <ntvy2k@gmail.com>
2023-08-24 09:10:22 +0700

    improve UI

--- app/main/pw-gen/page.tsx
+++ app/main/pw-gen/page.tsx
@@ -5,6 +5,9 @@ import { FormEvent, useState } from "react";
 import {
   Checkbox,
   Card,
+  CardHeader,
+  CardFooter,
+  CardBody,
   Typography,
   Button,
   Textarea,
@@ -42,7 +45,12 @@ export default function Page() {
       setState({ ...state, special: !state.special });
     },
     len: (ev: FormEvent<HTMLInputElement>) => {
-      setState({ ...state, len: Number(ev.currentTarget.value) });
+      let n = Number(ev.currentTarget.value);
+
+      n = n > 300 ? 300 : n;
+      n = n < 0 ? 0 : n;
+
+      setState({ ...state, len: n });
     },
   };
 
@@ -56,98 +64,108 @@ export default function Page() {
   };
 
   return (
-    <Card color="transparent" shadow={false}>
-      <Typography variant="h4" color="blue-gray">
-        Password Generator
-      </Typography>
-      <Typography color="gray" className="mt-1 font-normal">
-        Let generate your strong password
-      </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-        <Input
-          label="Length"
-          crossOrigin={""}
-          value={state.len}
-          type="number"
-          onInput={UpdateState.len}
-        />
-        <Checkbox
-          crossOrigin={""}
-          checked={state.lower}
-          onChange={UpdateState.lower}
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              Lowercase
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Checkbox
-          crossOrigin={""}
-          checked={state.upper}
-          onChange={UpdateState.upper}
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              Uppercase
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Checkbox
-          crossOrigin={""}
-          checked={state.digits}
-          onChange={UpdateState.digits}
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              Digits
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <Checkbox
-          crossOrigin={""}
-          checked={state.special}
-          onChange={UpdateState.special}
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              Special
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <div className="w-96">
-          <Textarea label="Password" value={password} onChange={() => {}} />
+    <Card color="transparent" shadow={false} className="h-full w-full">
+      <CardHeader floated={false} shadow={false} className="rounded-none">
+        <div className="mb-8 flex flex-col items-center justify-between gap-8">
+          <Typography variant="h4" color="blue-gray">
+            Password Generator
+          </Typography>
+          <Typography color="gray" className="mt-1 font-normal">
+            Let generate your strong password
+          </Typography>
         </div>
-        <div className="flex">
-          <Button className="mt-6" onClick={perform_generate}>
-            Generate
-          </Button>
-          <Button
-            className="mt-6"
-            onClick={() => {
-              navigator.clipboard.writeText(password);
-            }}
-          >
-            Copy
-          </Button>
+      </CardHeader>
+      <CardBody className="flex flex-col justify-center items-center">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 flex flex-col items-center gap-2">
+          <Input
+            label="Length"
+            crossOrigin={""}
+            value={state.len}
+            type="number"
+            onInput={UpdateState.len}
+          />
+          <div className="flex gap-4">
+            <Checkbox
+              crossOrigin={""}
+              checked={state.lower}
+              onChange={UpdateState.lower}
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
+                >
+                  Lowercase
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+            <Checkbox
+              crossOrigin={""}
+              checked={state.upper}
+              onChange={UpdateState.upper}
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
+                >
+                  Uppercase
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+            <Checkbox
+              crossOrigin={""}
+              checked={state.digits}
+              onChange={UpdateState.digits}
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
+                >
+                  Digits
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+            <Checkbox
+              crossOrigin={""}
+              checked={state.special}
+              onChange={UpdateState.special}
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
+                >
+                  Special
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            />
+          </div>
+          <div className="w-full flex justify-between">
+            <Button className="mt-6" onClick={perform_generate}>
+              Generate
+            </Button>
+            <Button
+              className="mt-6"
+              onClick={() => {
+                navigator.clipboard.writeText(password);
+              }}
+            >
+              Copy
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+      <CardFooter className="flex flex-col text-center items-center bg-gray-100">
+        <div className="overflow-y">
+          <Typography>{password}</Typography>
         </div>
-      </form>
+      </CardFooter>
     </Card>
   );
 }
