f2ea8b7719622b8ada4ec7096e6213178efbf128
VyNT <vyngt@outlook.com>
2025-08-09 22:35:32 +0700

    well function

--- pm-ui/src/components/button.rs
+++ pm-ui/src/components/button.rs
@@ -46,27 +46,29 @@ pub fn Button(
         ButtonEffect::None => {}
     };
 
+    let handle_style = move || {
+        let c = color.get();
+        let text_color = c.calculate_white_black_text_color(None);
+        let blend_color = c.calculate_white_black_text_color(Some(0.8));
+
+        let bg_color_var = format!("--background-color: rgb({}, {}, {})", c.r, c.g, c.b);
+        let text_color_var = format!(
+            "--text-color: rgb({}, {}, {})",
+            text_color.r, text_color.g, text_color.b
+        );
+        let text_color_80_var = format!(
+            "--text-color-80: rgb({}, {}, {})",
+            blend_color.r, blend_color.g, blend_color.b
+        );
+        format!("{};{};{};", bg_color_var, text_color_var, text_color_80_var)
+    };
+
     view! {
         <button
             type="button"
             class=cls
             on:click=handle_on_click
-            style=move || {
-                let c = color.get();
-                let text_color = c.calculate_white_black_text_color(None);
-                let blend_color = c.calculate_white_black_text_color(Some(0.8));
-
-                let bg_color_var = format!("--background-color: rgb({}, {}, {})", c.r, c.g, c.b);
-                let text_color_var = format!(
-                    "--text-color: rgb({}, {}, {})",
-                    text_color.r, text_color.g, text_color.b
-                );
-                let text_color_80_var = format!(
-                    "--text-color-80: rgb({}, {}, {})",
-                    blend_color.r, blend_color.g, blend_color.b
-                );
-                format!("{};{};{};", bg_color_var, text_color_var, text_color_80_var)
-            }
+            style=handle_style
         >
             {children()}
         </button>
