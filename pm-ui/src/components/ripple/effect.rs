8197c99acdb1a52cfa4378b80ec2968498c21e42
VyNT <vyngt@outlook.com>
2025-08-10 21:58:57 +0700

    enhance ripple

--- pm-ui/src/components/ripple/effect.rs
+++ pm-ui/src/components/ripple/effect.rs
@@ -1,7 +1,11 @@
 use leptos::prelude::*;
+use std::sync::atomic::{AtomicU64, Ordering};
+use wasm_bindgen::closure::Closure;
 use web_sys::wasm_bindgen::JsCast;
 
 use crate::types::color::RgbColor;
+
+static RIPPLE_COUNTER: AtomicU64 = AtomicU64::new(0);
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct RippleColor {
@@ -22,6 +26,10 @@ pub fn add_ripple(ev: web_sys::MouseEvent, color: Option<RippleColor>) {
         .unwrap()
         .create_element("span")
         .unwrap();
+
+    // Generate unique ID for this ripple
+    let ripple_id = format!("ripple-{}", RIPPLE_COUNTER.fetch_add(1, Ordering::Relaxed));
+    ripple.set_id(&ripple_id);
 
     ripple
         .dyn_ref::<web_sys::HtmlElement>()
@@ -72,10 +80,36 @@ pub fn add_ripple(ev: web_sys::MouseEvent, color: Option<RippleColor>) {
         .set_property("top", &format!("{y}px"))
         .unwrap();
 
-    // Remove old ripple if exists
-    if let Some(old) = target.query_selector(".ripple").unwrap() {
-        target.remove_child(&old).ok();
-    }
+    // Clean up old completed ripples (older than 1 second)
+    cleanup_old_ripples(&target);
 
     target.append_child(&ripple).unwrap();
+
+    // Set up automatic cleanup using animation end event
+    let target_clone = target.clone();
+    let ripple_id_clone = ripple_id.clone();
+
+    // Add animation end event listener for automatic cleanup
+    let closure = Closure::wrap(Box::new(move |_: web_sys::Event| {
+        if let Some(ripple_element) = target_clone
+            .query_selector(&format!("#{}", ripple_id_clone))
+            .unwrap()
+        {
+            target_clone.remove_child(&ripple_element).ok();
+        }
+    }) as Box<dyn FnMut(web_sys::Event)>);
+
+    ripple
+        .add_event_listener_with_callback("animationend", closure.as_ref().unchecked_ref())
+        .unwrap();
+
+    // Keep the closure alive
+    closure.forget();
+}
+
+fn cleanup_old_ripples(target: &web_sys::HtmlElement) {
+    // Remove any ripples that don't have an ID (legacy ripples)
+    if let Some(old_ripple) = target.query_selector(".ripple:not([id])").unwrap() {
+        target.remove_child(&old_ripple).ok();
+    }
 }
