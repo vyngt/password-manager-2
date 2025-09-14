fdc3ac1b9022087f0f10807d35095a0ca8896457
VyNT <vyngt@outlook.com>
2025-09-15 00:25:01 +0700

    seeding data

--- backend/src/v2/infra/data/sqlite/datasource/migration/db_theme/m20250914_000003_seeding_color_scheme.rs
+++ backend/src/v2/infra/data/sqlite/datasource/migration/db_theme/m20250914_000003_seeding_color_scheme.rs
@@ -1,5 +1,8 @@
-use super::entities::ColorScheme;
+use crate::v2::infra::data::sqlite::entities::theme::color_scheme;
+use chrono::Utc;
 use sea_orm_migration::prelude::*;
+use sea_orm_migration::sea_orm::entity::*;
+use uuid::uuid;
 
 pub struct Migration;
 
@@ -9,15 +12,39 @@ impl MigrationName for Migration {
     }
 }
 
-async fn generate_records() {}
-
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        let vortex = color_scheme::ActiveModel {
+            id: Set(uuid!("14368642-5c48-4aa6-8a5c-fe7346e4cd1e")),
+            name: Set("Vortex".to_string()),
+            color_primary: Set("#F41FC6".to_string()),
+            color_secondary: Set("#03A9F4".to_string()),
+            color_success: Set("#1EC887".to_string()),
+            color_danger: Set("#F42563".to_string()),
+            color_warning: Set("#E1C124".to_string()),
+            color_foreground: Set("#FFFFFF".to_string()),
+            color_background: Set("#000000".to_string()),
+            created_at: Set(Utc::now()),
+            updated_at: Set(Utc::now()),
+        };
+
+        color_scheme::Entity::insert_many(vec![vortex])
+            .exec(db)
+            .await?;
+
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        color_scheme::Entity::delete_by_id(uuid!("14368642-5c48-4aa6-8a5c-fe7346e4cd1e"))
+            .exec(db)
+            .await?;
+
         Ok(())
     }
 }
